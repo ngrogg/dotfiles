ffb15e58e9faf1e89da397dcd06fe08a3c3f757b
Nicholas Grogg <ngrogg@pop-os.localdomain>
2023-05-21 01:40:05 -1000

    Updated PowerShell Profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,19 @@
 # PowerShell RC file
-# ~/.config/powershell/Microsoft.PowerShell_profile.ps1
-set-alias cat get-content
+# By Nicholas Grogg
+
+# Configure autocomplete to function more like ZSH
+Import-Module Microsoft.Powershell.UnixCompleters
+Import-Module Microsoft.PowerShell.UnixTabCompletion
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+Import-UnixCompleters
+
+# Append to path
+$env:Path += ":~/bin"
+
+# Aliases
+set-alias cat "get-content"
+function gits {
+	chdir ~/Documents/gits
+	dir }
+function ll {
+	bash -c "ls -l" }
