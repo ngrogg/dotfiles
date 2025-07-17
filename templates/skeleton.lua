dfcd4c5eebced7b97f10131b8b23f0f2537939f7
ngrogg <nicholas_grogg6505@hotmail.com>
2025-07-17 13:09:39 -1000

    Added file templates/skeleton.lua.

---
+++ templates/skeleton.lua
@@ -0,0 +1,13 @@
+#!/usr/bin/lua
+
+-- Program Name
+-- Program Description
+-- By Nicholas Grogg
+
+-- Function to run program
+function runProgram()
+        print("Program Name!")
+end
+
+-- Run the function
+runProgram()
